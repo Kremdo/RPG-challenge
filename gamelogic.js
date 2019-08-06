a66dcad5fb1df1b6ee78814f3f0042d31e4a6dd4
Ben Senica <senicaben@gmail.com>
2019-08-06 15:24:10 +0200

    Added gamelogics.js

---
+++ gamelogic.js
@@ -0,0 +1,23 @@
+import Person from "/character.js";
+
+
+
+/*function item(chosenItem) {
+    var f = document.getElementById("item");
+    var chosenItem = f.options[f.selectedIndex].text;
+    if (chosenItem == "Boots") {
+        return chosenItem;
+
+    } else if (chosenItem == "Staff") {
+        return chosenItem;
+
+    } else if (chosenItem == "Sword") {
+        return chosenItem
+
+    } else {
+        chosenItem = "Bow";
+
+    }
+}
+
+*/
