571232c2179b6ebe998a4d2af685cbb314a9774f
Ben Senica <senicaben@gmail.com>
2019-08-07 10:31:57 +0200

    Added code for both character creation

--- gamelogic.js
+++ gamelogic.js
@@ -1,25 +1,37 @@
 import Person from "/character.js";
-var chosenRace;
-var chosenItem;
+var chosenRace1;
+var chosenItem1;
+var chosenRace2;
+var chosenItem2;
 document.getElementById("createbtn").addEventListener("click", create);
 
 function create() {
-    raceFunction();
-    itemFunction();
-    var createdChar = new Person(chosenRace, chosenItem);
-    document.getElementById("pRace1").innerHTML += chosenRace;
-    document.getElementById("pItem1").innerHTML += chosenItem;
-
-}
-
-
-
-function raceFunction() {
-    chosenRace = document.getElementById("race1").value;
+    raceFunctionOne();
+    itemFunctionOne();
+    raceFunctionTwo();
+    itemFunctionTwo();
+    var createdChar1 = new Person(chosenRace1, chosenItem1);
+    var createdChar2 = new Person(chosenRace2, chosenItem2);
+    document.getElementById("pRace1").innerHTML += chosenRace1;
+    document.getElementById("pItem1").innerHTML += chosenItem1;
+    document.getElementById("pRace2").innerHTML += chosenRace2;
+    document.getElementById("pItem2").innerHTML += chosenItem2;
 
 
 }
 
-function itemFunction() {
-    chosenItem = document.getElementById("item1").value;
+function raceFunctionOne() {
+    chosenRace1 = document.getElementById("race1").value;
+}
+
+function itemFunctionOne() {
+    chosenItem1 = document.getElementById("item1").value;
+}
+
+function raceFunctionTwo() {
+    chosenRace2 = document.getElementById("race2").value;
+}
+
+function itemFunctionTwo() {
+    chosenItem2 = document.getElementById("item2").value;
 }
