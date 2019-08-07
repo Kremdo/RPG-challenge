efb9cb4cbd0c74b7188598724fa5b2a745b9b72a
Ben Senica <senicaben@gmail.com>
2019-08-07 10:22:40 +0200

    added some js magic

--- gamelogic.js
+++ gamelogic.js
@@ -1,14 +1,25 @@
 import Person from "/character.js";
+var chosenRace;
+var chosenItem;
+document.getElementById("createbtn").addEventListener("click", create);
 
-document.getElementById("createbtn").addEventListener("click", race);
+function create() {
+    raceFunction();
+    itemFunction();
+    var createdChar = new Person(chosenRace, chosenItem);
+    document.getElementById("pRace1").innerHTML += chosenRace;
+    document.getElementById("pItem1").innerHTML += chosenItem;
 
-
-function race() {
-    var chosenRace = document.getElementById("race").value;
-    console.log(chosenRace);
 }
 
-function item() {
-    var chosenItem = document.getElementById("item").value;
-    console.log(chosenItem);
+
+
+function raceFunction() {
+    chosenRace = document.getElementById("race1").value;
+
+
+}
+
+function itemFunction() {
+    chosenItem = document.getElementById("item1").value;
 }
