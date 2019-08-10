ee17ed82e5a470a9c77a2612e2b151d6fd046984
Ben Senica <senicaben@gmail.com>
2019-08-10 11:00:35 +0200

    Added winner alerts

--- character.js
+++ character.js
@@ -8,6 +8,7 @@ import {
 var healthPercentage1;
 var healthPercentage2;
 
+
 //Use this script to generate your character
 export default function Person(race, item) {
     this.race = race;
@@ -18,7 +19,9 @@ export default function Person(race, item) {
     this.min = 3;
     this.maxDamage = 20;
     this.maxHealing = 30;
-    
+
+    var p1Name = document.getElementById("player1Name").value;
+    var p2Name = document.getElementById("player2Name").value;
 
     //healing function
     this.heal = function () {
@@ -80,7 +83,7 @@ export default function Person(race, item) {
         //calculate var for all chance percentages
         var chance = Math.random();
 
-        
+
 
         //check if player 1 pushes button
         if (event.target == document.getElementById("phit1")) {
@@ -89,23 +92,20 @@ export default function Person(race, item) {
                 createdChar1.currenthealth = createdChar1.currenthealth + Math.floor(createdChar2.currenthealth * 0.1);
                 createdChar2.currenthealth = createdChar2.currenthealth - Math.floor(createdChar2.currenthealth * 0.1);
                 healthPercentage1 = createdChar1.currenthealth * (100 / createdChar1.maxHealth);
-                console.log(createdChar1.currenthealth);
-                console.log(createdChar2.currenthealth);
                 if (createdChar1.currenthealth > createdChar1.maxHealth) {
                     createdChar1.currenthealth = createdChar1.maxHealth;
                     healthPercentage1 = createdChar1.currenthealth * (100 / createdChar1.maxHealth);
-                                       
+
                 };
-                console.log(createdChar1.currenthealth);
                 document.getElementById("bar1").innerHTML = createdChar1.currenthealth;
                 document.getElementById("bar1").style.width = healthPercentage1 + "%";
             }
-            
+
             //calculate random damage between max and min
             var randomDamage = Math.floor(Math.random() * (createdChar1.maxDamage - createdChar1.min + 1) + createdChar1.min);
-            
-            
-           
+
+
+
 
             //check what item player 1 is holding
             switch (createdChar1.item) {
@@ -125,7 +125,6 @@ export default function Person(race, item) {
 
                             document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
                             document.getElementById("bar2").style.width = healthPercentage2 + "%";
-                            console.log(randomDamage);
 
                             i++;
 
@@ -156,26 +155,18 @@ export default function Person(race, item) {
             }
 
             if (createdChar2.race == "elves") {
-                console.log(randomDamage);
                 if (chance < 0.3) {
                     randomDamage = Math.ceil(randomDamage * 0.5);
-                    console.log(randomDamage);
-                    console.log("reflected damage " + randomDamage);
                     createdChar1.currenthealth -= randomDamage;
                     healthPercentage1 = createdChar1.currenthealth * (100 / createdChar1.maxHealth)
                     document.getElementById("bar1").innerHTML = createdChar1.currenthealth;
                     document.getElementById("bar1").style.width = healthPercentage1 + "%";
-                    
-                    console.log("P1 current health " + createdChar1.currenthealth);
-                    randomDamage = 0;
-                    console.log(randomDamage);
 
                 } else {
                     createdChar2.currenthealth -= randomDamage;
                     healthPercentage2 = createdChar2.currenthealth * (100 / createdChar2.maxHealth);
                     document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
                     document.getElementById("bar2").style.width = healthPercentage2 + "%";
-                    console.log("P2 curr health" + createdChar2.currenthealth);
                 }
             }
 
@@ -184,7 +175,16 @@ export default function Person(race, item) {
             document.getElementById("pic2").style.width = healthPercentage2 + "%";
             document.getElementById("pic2").style.height = healthPercentage2 + "%";
 
-    
+            //check if player 2 hits 0 hp and pronounce winner is so
+            if (createdChar2.currenthealth <= 0) {
+                createdChar2.currenthealth = 0;
+                healthPercentage2 = 0;
+                document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
+                document.getElementById("bar2").style.width = healthPercentage2 + "%";
+
+                alert(p2Name + " is knocked out!\n" + p1Name + " wins");
+                location.reload();
+            }
 
             //check if player 2 hits button
         } else if (event.target == document.getElementById("phit2")) {
@@ -195,18 +195,16 @@ export default function Person(race, item) {
                 createdChar2.currenthealth = createdChar2.currenthealth + Math.floor(createdChar1.currenthealth * 0.1);
                 createdChar1.currenthealth = createdChar1.currenthealth - Math.floor(createdChar1.currenthealth * 0.1);
                 healthPercentage2 = createdChar2.currenthealth * (100 / createdChar2.maxHealth);
-                console.log(createdChar1.currenthealth);
-                console.log(createdChar2.currenthealth);
+
                 if (createdChar2.currenthealth > createdChar2.maxHealth) {
                     createdChar2.currenthealth = createdChar2.maxHealth;
                     healthPercentage2 = createdChar2.currenthealth * (100 / createdChar2.maxHealth);
-                                       
+
                 };
-                console.log(createdChar2.currenthealth);
                 document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
                 document.getElementById("bar2").style.width = healthPercentage2 + "%";
             }
-            
+
 
             //check what item player 2 is using 
             switch (createdChar2.item) {
@@ -221,16 +219,15 @@ export default function Person(race, item) {
                         let i = 0;
                         while (i < 1) {
 
-                            createdChar2.currenthealth -= randomDamage;
-                            healthPercentage2 = createdChar2.currenthealth * (100 / createdChar2.maxHealth);
+                            createdChar1.currenthealth -= randomDamage;
+                            healthPercentage1 = createdChar1.currenthealth * (100 / createdChar1.maxHealth);
 
-                            document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
-                            document.getElementById("bar2").style.width = healthPercentage2 + "%";
-                            console.log(randomDamage);
+                            document.getElementById("bar1").innerHTML = createdChar1.currenthealth;
+                            document.getElementById("bar1").style.width = healthPercentage2 + "%";
 
                             i++;
 
-                            randomDamage = Math.floor(Math.random() * (createdChar1.maxDamage - createdChar1.min + 1) + createdChar1.min);
+                            randomDamage = Math.floor(Math.random() * (createdChar2.maxDamage - createdChar2.min + 1) + createdChar2.min);
                         }
                     }
                     break;
@@ -248,7 +245,7 @@ export default function Person(race, item) {
 
 
             //calculate done damage and push to html
-            
+
             if (createdChar1.race == "humans") {
                 randomDamage -= Math.ceil(randomDamage * 0.2);
 
@@ -260,19 +257,13 @@ export default function Person(race, item) {
             }
 
             if (createdChar1.race == "elves") {
-                console.log(randomDamage);
                 if (chance < 0.3) {
                     randomDamage = Math.ceil(randomDamage * 0.5);
-                    console.log(randomDamage);
-                    console.log("reflected damage " + randomDamage);
+
                     createdChar2.currenthealth -= randomDamage;
                     healthPercentage2 = createdChar2.currenthealth * (100 / createdChar2.maxHealth)
                     document.getElementById("bar2").innerHTML = createdChar2.currenthealth;
                     document.getElementById("bar2").style.width = healthPercentage2 + "%";
-                    
-                    console.log("P2 current health " + createdChar2.currenthealth);
-                    //randomDamage = 0;
-                    console.log(randomDamage);
 
                 } else {
                     createdChar1.currenthealth -= randomDamage;
@@ -289,19 +280,25 @@ export default function Person(race, item) {
             document.getElementById("pic1").style.width = healthPercentage1 + "%";
             document.getElementById("pic1").style.height = healthPercentage1 + "%";
 
+            //check if player 1 hits 0 hp and pronounce winner is so
+            if (createdChar1.currenthealth <= 0) {
+                createdChar1.currenthealth = 0;
+                healthPercentage1 = 0;
+                document.getElementById("bar1").innerHTML = createdChar1.currenthealth;
+                document.getElementById("bar1").style.width = healthPercentage1 + "%";
+
+                alert(p1Name + " is knocked out!\n" + p2Name + " wins");
+                location.reload();
+            }
+
 
         }
 
-        /*if (createdChar1.currenthealth <= 0) {
-            createdChar1.currenthealth = 0;
-            healthPercentage1 = 0;
-            document.getElementById("bar1").innerHTML = createdChar1.currenthealth;
-            document.getElementById("bar1").style.width = healthPercentage1 + "%";
-            alert (p1Name);
-        }*/
-        
-    };
-    
+
+    }
+
+
+
     this.totalDamage = this.damage();
 
     displayChar(this.race, this.item, this.maxHealth);
